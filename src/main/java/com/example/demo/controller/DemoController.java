6a5efb48403c389e9b1df5266da2e26f3ee18538
ZibrasIsmail <zibrasismail@gmail.com>
2021-09-26 15:47:34 +0530

    first commit

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @Value("${demo.prop}")
+    private String message;
+
+    @GetMapping
+    public ResponseEntity<String> getDemoResponse(){
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+
+}
+
+
